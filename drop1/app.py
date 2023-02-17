746d06348b0f1bda22448b769c6fae8a7baf4ed1
William Tang <48343678+WiIIiamTang@users.noreply.github.com>
2023-02-17 00:10:29 -0500

    add communication between middlefs

--- drop1/app.py
+++ drop1/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -6,3 +8,41 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<h3>drop-1 compute</h3>"
+
+
+@app.route("/heartbeat", methods=["POST"])
+def heartbeat():
+    token = request.headers.get("Authorization")
+    bodydata = request.get_json()
+    if type(bodydata) is not dict:
+        try:
+            bodydata = json.loads(bodydata)
+        except Exception as e:
+            print(e)
+            return "Invalid body data", 400
+
+    if not token or not bodydata:
+        return "Invalid request", 400
+
+    ENDPOINT = "https://fleet.williamtang.me/api"
+
+    r = requests.get(
+        f"{ENDPOINT}/fleet/service/directive?discordAccountId={bodydata.get('shared_id')}",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {token}",
+        },
+    )
+    print(
+        "drop1 receiving heartbeat check:",
+        bodydata.get("dname")
+        in [
+            d.get("name")
+            for d in r.json().get("directives")
+            if d.get("serviceId") == bodydata.get("serviceId")
+        ],
+    )
+    if not r.json().get("success"):
+        return "Unauthorized", 401
+
+    return "OK", 200
--- pi/boot.py
+++ pi/boot.py
@@ -1,2 +1,110 @@
 import requests
+import argparse
 import json
+from subprocess import Popen, PIPE
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--key", "-k", help="path to key file")
+    parser.add_argument(
+        "--root", "-r", help="abs path to root directory of this git repo"
+    )
+    args = parser.parse_args()
+
+    keydata = {}
+    with open(args.key) as f:
+        keydata = json.load(f)
+
+    ENDPOINT = "https://fleet.williamtang.me/api"
+    status = {"connected": False, "authorized": False}
+
+    r = requests.post(
+        f"{ENDPOINT}/fleet/service/connectuser",
+        json={
+            "discordAccountId": keydata.get("shared_id"),
+            "discordUsername": keydata.get("shared_idname"),
+            "serviceId": keydata.get("serviceId"),
+            "serviceNameIdentifier": keydata.get("serviceNameIdentifier"),
+        },
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f'Bearer {keydata.get("AUTH_KEY")}',
+        },
+    )
+    status["connected"] = r.json().get("success")
+
+    r = requests.get(
+        f"{ENDPOINT}/fleet/service/directive?discordAccountId={keydata.get('shared_id')}",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f'Bearer {keydata.get("AUTH_KEY")}',
+        },
+    )
+    status["authorized"] = keydata.get("dname") in [
+        d.get("name")
+        for d in r.json().get("directives")
+        if d.get("serviceId") == keydata.get("serviceId")
+    ]
+    print(status)
+
+    if not status.get("connected") or not status.get("authorized"):
+        return
+
+    r = requests.get(
+        f"{ENDPOINT}/fleet/stats?serviceId={keydata.get('serviceId')}&serviceNameIdentifier={keydata.get('serviceNameIdentifier')}&discordAccountId={keydata.get('shared_id')}&statCategory=_fs_ping&statDescription=_fs_ping&statName=_fs_ping_connect",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f'Bearer {keydata.get("AUTH_KEY")}',
+        },
+    )
+    fsp_value = r.json().get("stats")[0].get("statValue")
+
+    r = requests.patch(
+        f"{ENDPOINT}/fleet/stats",
+        json={
+            "serviceId": keydata.get("serviceId"),
+            "serviceNameIdentifier": keydata.get("serviceNameIdentifier"),
+            "discordAccountId": keydata.get("shared_id"),
+            "statCategory": "_fs_ping",
+            "statDescription": "_fs_ping",
+            "statValue": str(int(fsp_value) + 1),
+            "statName": "_fs_ping_connect",
+        },
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f'Bearer {keydata.get("AUTH_KEY")}',
+        },
+    )
+    if not r.json().get("success"):
+        raise RuntimeError("Failed to run patch request")
+
+    p = Popen(
+        [
+            "git",
+            "pull",
+        ],
+        cwd=args.root,
+        stdout=PIPE,
+        stderr=PIPE,
+    )
+    out, err = p.communicate()
+    print(str(out))
+    print(str(err))
+
+    r = requests.post(
+        keydata.get("drop1"),
+        json={
+            "dname": keydata.get("dname"),
+            "serviceId": keydata.get("serviceId"),
+            "shared_id": keydata.get("shared_id"),
+        },
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f'Bearer {keydata.get("AUTH_KEY")}',
+        },
+    )
+
+
+if __name__ == "__main__":
+    main()
